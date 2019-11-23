4a0ce78c749f5c9015298ee4460d99af9ec6d41d
Peter Keating <peter@moov2.com>
2019-11-23 12:46:49 +0000

    Create module using module boilerplate template

---
+++ Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Author = "Etch UK Ltd.",
+    Category = "Content",
+    Description = "Manage browser related icons (e.g. favicon, apple-touch-icon, etc...)",
+    Name = "Favicons",
+    Version = "0.0.1",
+    Website = "https://etchuk.com"
+)]
---
+++ README.md
@@ -0,0 +1,7 @@
+# Etch.OrchardCore.Favicon
+
+Manage browser related icons (e.g. favicon, apple-touch-icon, etc...)
+
+## Orchard Core Reference
+
+This module is referencing the RC1 build of Orchard Core ([`1.0.0-rc1-10004`](https://www.nuget.org/packages/OrchardCore.Module.Targets/1.0.0-rc1-10004)).
