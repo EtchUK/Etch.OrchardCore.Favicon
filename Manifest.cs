8b2881a77015586f9aae7680851353d78be1e9f3
Peter Keating <peter@moov2.com>
2019-11-26 18:57:58 +0000

    Update manifest description & version

--- Manifest.cs
+++ Manifest.cs
@@ -3,9 +3,9 @@ using OrchardCore.Modules.Manifest;
 [assembly: Module(
     Author = "Etch UK Ltd.",
     Category = "Content",
-    Description = "Manage browser related icons (e.g. favicon, apple-touch-icon, etc...)",
+    Description = "Manage browser related features (e.g. favicon, apple-touch-icon, browserconfig.xml, etc...)",
     Name = "Favicons",
-    Version = "0.0.1",
+    Version = "0.1.0",
     Website = "https://etchuk.com",
     Dependencies = new string[] { "OrchardCore.ContentFields", "OrchardCore.Media" }
 )]
