fa50f0357ac68066895c0509c43bfd4110eb73e6
Peter Keating <pete.keating@etchuk.com>
2021-01-14 12:17:49 +0000

    v0.2.1

--- Manifest.cs
+++ Manifest.cs
@@ -5,7 +5,7 @@ using OrchardCore.Modules.Manifest;
     Category = "Content",
     Description = "Manage browser related features (e.g. favicon, apple-touch-icon, browserconfig.xml, etc...)",
     Name = "Favicons",
-    Version = "0.2.0",
+    Version = "0.2.1",
     Website = "https://etchuk.com",
     Dependencies = new string[] { "OrchardCore.ContentFields", "OrchardCore.Media" }
 )]
