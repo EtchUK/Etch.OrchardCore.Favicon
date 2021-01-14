9458175a5d583865039df29fbe52d41c28fa0b81
Peter Keating <pete.keating@etchuk.com>
2021-01-14 12:16:01 +0000

    Set default mime type for .webmanifest file

--- DefaultMimeTypes.cs
+++ DefaultMimeTypes.cs
@@ -1,11 +1,11 @@
 ﻿namespace Etch.OrchardCore.Favicon
 {
-    public class DefaultMimeTypes
+    public static class DefaultMimeTypes
     {
         public const string AppleTouchIcon = "image/png";
         public const string BrowserConfig = "application/xml";
         public const string Favicon = "image/x-icon";
         public const string Tile = "image/png";
-        public const string WebAppManifest = "";
+        public const string WebAppManifest = "application/manifest+json";
     }
 }
