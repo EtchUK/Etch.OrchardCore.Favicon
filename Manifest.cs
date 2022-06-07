7c4be70c0525aa3199d7f01612596f4c636a4f5c
Mehdi Miah <Mehdi.Miah@etchuk.com>
2022-06-07 12:22:29 +0100

    Upgrade to orchard core 1.4.0

--- Manifest.cs
+++ Manifest.cs
@@ -5,7 +5,7 @@ using OrchardCore.Modules.Manifest;
     Category = "Content",
     Description = "Manage browser related features (e.g. favicon, apple-touch-icon, browserconfig.xml, etc...)",
     Name = "Favicons",
-    Version = "1.3.1",
+    Version = "1.4.0",
     Website = "https://etchuk.com",
     Dependencies = new string[] { "OrchardCore.ContentFields", "OrchardCore.Media" }
 )]
